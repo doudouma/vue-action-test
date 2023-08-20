31bd0963780fca5a8957bcffb05e56510dd30d3c
doudouma <ianni-k@163.com>
2023-08-20 17:19:10 +0800

    add search and info

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,28 +1,63 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
+    // search: {
+    //   provider: "local",
+    // },
+    algolia: {
+      appId: "1K6FEL350T", // 需要替换
+      apiKey: "21a4a77c64c2e01c96e104e72117e332", // 需要替换
+      indexName: "chodocs", // 需要替换
+      placeholder: "Search docs",
+      buttonText: "search",
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+      { text: "test", link: "/test" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "Examples",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+  head: [
+    [
+      "script",
+      {},
+      `
+      window.difyChatbotConfig = { token: 'OS9EyVJcFQR2C3vP' }
+      `,
+    ],
+    [
+      "script",
+      {
+        src: "https://udify.app/embed.min.js",
+        id: "OS9EyVJcFQR2C3vP",
+        defer: true,
+      },
+    ],
+    [
+      "script",
+      {},
+      `
+      setTimeout(()=>embedChatbot(),100)
+       `,
+    ],
+  ],
+});
